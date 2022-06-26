971ca61aa1f3b619dbe7f21804270e6cbcd2103f
lecastroMELI <leticia.castro@mercadolivre.com>
2022-06-26 08:49:26 -0300

    Implementado o método ehMaiorIdade() na classe Pessoa

--- src/exercicios/Pessoa.java
+++ src/exercicios/Pessoa.java
@@ -26,11 +26,15 @@ public class Pessoa {
         if (imc < 20) {
             return -1;
         } else {
-            if(imc >= 20 && imc <= 25){
+            if (imc >= 20 && imc <= 25) {
                 return 0;
             } else {
                 return -1;
             }
         }
+    };
+
+    boolean ehMaioIdade(int age) {
+        return age >= 18;
     };
 }
