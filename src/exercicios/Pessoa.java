98a15671e0adb2929da1dc792a76dce775c54c40
lecastroMELI <leticia.castro@mercadolivre.com>
2022-06-26 07:31:32 -0300

    Criada classe Pessoa e implementados 3 construtores

---
+++ src/exercicios/Pessoa.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+public class Pessoa {
+    // atributos = características
+    String id;
+    String name;
+    int age;
+    double weight;
+    double height;
+
+    Pessoa() {
+
+    };
+
+    Pessoa(String id, String name, int age) {
+
+    };
+
+    Pessoa(String id, String name, int idade, double weight, double height) {
+
+    };
+}
