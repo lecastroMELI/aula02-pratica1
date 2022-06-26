9c491a9c93d60245d4579148c5a64385d4fee03f
lecastroMELI <leticia.castro@mercadolivre.com>
2022-06-26 08:22:34 -0300

    Implementado a classe Main e seus 3 objetos tipo Pessoa

---
+++ src/exercicios/Main.java
@@ -0,0 +1,9 @@
+package exercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa("A321","Dyana Prince", 35);
+        Pessoa pessoa3 = new Pessoa();
+    }
+}
